5302fd09bc7478e9da9ef42ecbae85177a334d94
ChasingRain <junpeng94@gmail.com>
2022-07-26 11:59:18 +0800

    post JSON data to server and read server resp when creating account and navigate user to main page after login

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,7 @@
 import React, {useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import {toast } from 'react-toastify';
+import axios from 'axios';
 import 'react-toastify/dist/ReactToastify.css'
 
 function SignUp() {
@@ -12,33 +13,27 @@ function SignUp() {
     const [confirmUserPassword, setConfirmUserPassword] = useState("")
     const [error, setError] = useState("")
     const [signUpBtnLoading, setSignUpBtnLoading] = useState("")
+    const navigate = useNavigate()
 
+    // form validation logic
     const validateForm = (e) => {
-        console.log("submit button pressed!")
-        console.log("userEmail: ", userEmail)
-        console.log("firstName: ", firstName)
-        console.log("lastName: ", lastName)
-        console.log("userPassword: ", userPassword)
-        console.log("confirmUserPassword: ", confirmUserPassword)
+        
+        e.preventDefault()
 
         if(!validateEmail(userEmail)) {
             toast.warn("email is invalid!")
-            e.preventDefault()
             return false
         }
         if(!firstName){ 
             toast.warn("First name cannot be empty!")
-            e.preventDefault()
             return false
         }
         if(!lastName){ 
             toast.warn("Last name cannot be empty!")
-            e.preventDefault()
             return false
         }
         if(userPassword.length < 8){ 
             toast.warn("Password must be at least 8 characters long!")
-            e.preventDefault()
             return false
         }
         if(userPassword !== confirmUserPassword){ 
@@ -46,6 +41,7 @@ function SignUp() {
             e.preventDefault()
             return false
         }
+        sendFormDataToServer()
     }
 
     const validateEmail = (email) => {
@@ -61,13 +57,22 @@ function SignUp() {
         setConfirmUserPassword("")
     }
 
+    const sendFormDataToServer = async () => {
+        const res = await axios.post(
+            "http://api.sideprojectschool.com:3000/api/user/create", 
+            { email: userEmail, first_name: firstName, last_name: lastName, password: userPassword, password_confirm: confirmUserPassword}
+        );
+        console.log(res)
+        if(res.status === 200) navigate('/')
+    }
+
     return (
     <div>
         <div className="flex flex-col w-full border-opacity-50">
             <div className="grid card rounded-box place-items-center mt-32">
                 <h1 className='text-6xl my-6'>Messenger</h1>
                     {/* <form className="form-control w-full max-w-xs" onSubmit={validateForm}> */}
-                    <form className="form-control w-full max-w-xs" action="http://api.sideprojectschool.com:3000/api/user/create" method="post" onSubmit={validateForm}>
+                    <form className="form-control w-full max-w-xs" onSubmit={validateForm}>
                         <input type="text" name="email" placeholder="Email address" className="input input-bordered w-full max-w-xs my-2" onChange={(event) => setUserEmail(event.target.value)} value={userEmail}/>
                         <input type="text" name="first_name" placeholder="First name / Given name" className="input input-bordered w-full max-w-xs my-2" onChange={(event) => setFirstName(event.target.value)} value={firstName}/>
                         <input type="text" name="last_name" placeholder="Last name / Family name" className="input input-bordered w-full max-w-xs my-2" onChange={(event) => setLastName(event.target.value)} value={lastName}/>
