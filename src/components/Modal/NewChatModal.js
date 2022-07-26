49616b76e51dd0e47af0a0e96ab542e7c086c6a6
ChasingRain <junpeng94@gmail.com>
2022-07-26 23:31:27 +0800

    added map method to start new chat component

--- src/components/Modal/NewChatModal.js
+++ src/components/Modal/NewChatModal.js
@@ -4,6 +4,27 @@ import BubbleProfile from '../BubbleProfile'
 import SeachBar from '../SeachBar'
 
 function NewChatModal({friendList}) {
+    const fakeFriendList = [
+        {
+            "user_id": "abcdef",
+            "first_name": "Kyle",
+            "last_name": "Jung",
+            "prof_pic_url": "url"
+        },
+        {
+            "user_id": "aasfdsdafa",
+            "first_name": "Sam",
+            "last_name": "Ha",
+            "prof_pic_url": "url"
+        },
+        {
+            "user_id": "asdfne",
+            "first_name": "Sepjo",
+            "last_name": "Choi",
+            "prof_pic_url": "url"
+        }
+    ]
+
     return (
         <div>
             {/* <!-- The button to open modal --> */}
@@ -16,12 +37,14 @@ function NewChatModal({friendList}) {
                     <label for="my-modal-2" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 class="text-lg font-bold mb-4">Start a new chat with:</h3>
                     <SeachBar placeholder="Search by email or username"/>
-                    <div className='card-body bg-slate-400 py-2 mt-2 rounded-md'>
-                        <BubbleProfile initial="JC" name="John Cena" bubbleWidth="w-14" nameTextSize="text-lg"/>
-                    </div>
-                    <div className='card-body bg-slate-400 py-2 mt-2 rounded-md'>
-                        <BubbleProfile initial="JC" name="John Cena" bubbleWidth="w-14" nameTextSize="text-lg"/>
-                    </div>
+                    {
+                        fakeFriendList.map((friend, index) => (
+                            <div className='card-body bg-slate-400 py-2 mt-2 rounded-md'>
+                                <BubbleProfile initial={friend.first_name} name={friend.first_name} bubbleWidth="w-14" nameTextSize="text-lg"/>
+                            </div>
+                        ))
+                        // friendList.map((friend, index) => <BubbleProfile initial="JC" name="John Cena" bubbleWidth="w-14" nameTextSize="text-lg"/>)
+                    }
                     <div className='card-body bg-slate-400 py-2 mt-2 rounded-md'>
                         <BubbleProfile initial="JC" name="John Cena" bubbleWidth="w-14" nameTextSize="text-lg"/>
                     </div>
